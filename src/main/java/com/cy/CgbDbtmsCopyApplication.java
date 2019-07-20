a4bddafad21cae9f1837c859be3fd50b57e79d78
ZhaoShuaiNuLi <zhaoshuai0822@163.com>
2019-07-20 09:22:25 +0800

    zhaoshuai-test

--- src/main/java/com/cy/CgbDbtmsCopyApplication.java
+++ src/main/java/com/cy/CgbDbtmsCopyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CgbDbtmsCopyApplication {
-	//menghao-test-push
+	//test-test-push
 	public static void main(String[] args) {
 		SpringApplication.run(CgbDbtmsCopyApplication.class, args);
 	}
